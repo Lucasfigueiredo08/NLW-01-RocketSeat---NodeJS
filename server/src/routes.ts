774bbbbc45343d093f279ca6d6b22fa88dbacfc2
Lucas Figueiredo <lukas.figueiredo14@gmail.com>
2020-08-31 23:59:08 -0300

    Aula 02 - 1h 23m

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import Knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Hello World'});
-}); 
+routes.get('/items', async (request, response) => {
+    const items = await Knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+        return { // retornar o item no formato title, image_url
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        }
+    });
+
+    return response.json(serializedItems);
+}); // pegar uma lista
 
 export default routes;
